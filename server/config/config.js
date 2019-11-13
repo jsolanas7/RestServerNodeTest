5e1a8a1253876c24746471c584b93e546aa926e2
Juan Andrés Solanas <jsolanas@mstech.la>
2019-11-13 08:59:41 -0300

    add MONGO_URI process variable

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    urlDB = 'mongodb+srv://admin:admin@cluster0-k9gxz.mongodb.net/test'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
