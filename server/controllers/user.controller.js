f5aac911d3d5911197f2604549e8d7a58b7391e0
Juan Andrés Solanas <jsolanas@mstech.la>
2019-11-20 16:54:16 -0300

    Add relation 1 to many with users and address

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -17,20 +17,14 @@ app.post('/user/create', async function (req, res) {
         password: bcrypt.hashSync(body.password, 10),
         role: body.role,
     });
-    const list = [];
-    if(body.address){
-        body.address.forEach(function(item){
-            const address = new Address({
-                name: item.name,
-                user: user._id
-            });
-            list.push(address);
-        })
-    }
     try {
-        
+        if (body.addresses) {
+            const newAddresses = await Address.insertMany(body.addresses);
+        }
+        newAddresses.forEach(function (item) {
+            user.addresses.push(item._id);
+        })
         const newUser = await user.save();
-        const newAddresses = await Address.insertMany(list);
         res.json({
             ok: true,
             user: newUser
@@ -38,12 +32,12 @@ app.post('/user/create', async function (req, res) {
     } catch (err) {
         console.log(err);
         return res.status(400).json({
-                        ok: false,
-                        err
-                    })
+            ok: false,
+            err
+        })
     }
 });
-app.put('/user/update/:id', [ validatorToken, validatorAdminRole ],async function (req, res) {
+app.put('/user/update/:id', [validatorToken, validatorAdminRole], async function (req, res) {
     const id = req.params.id;
     // let body = _.pick(req.body, ['name', 'email', 'role', 'status']);
     const body = req.body;
@@ -64,27 +58,27 @@ app.put('/user/update/:id', [ validatorToken, validatorAdminRole ],async functio
     }
 });
 
-app.get('/user/getall', [validatorToken ], async function (req, res) {
+app.get('/user/getall', [validatorToken], async function (req, res) {
     res.header(this.headerOptions);
     const limit = Number(req.query.limit) || 5;
     const skip = Number(req.query.skip) || 0;
     try {
         const that = this;
-        const users = await User.find({ status: true }, 'name email role google address')
-            .populate({ path:"role",
-                        populate: { path: 'group'}
-                    })
+        const users = await User.find({ status: true }, 'name email role google addresses')
+            .lean()
+            .populate({
+                path: "role",
+                populate: { path: 'group' }
+            })
+            .populate({ path: 'addresses' })
             .skip(skip)
             .limit(limit)
             .exec();
-            const addresses = await Address.find({user : entity._id})
-                .exec();
 
-        res.json({ 
+
+        res.json({
             ok: true,
             users,
-            address,
-            count,
             userGet: req.userToken
         })
     } catch (err) {
@@ -97,22 +91,29 @@ app.get('/user/getall', [validatorToken ], async function (req, res) {
         })
     }
 });
-app.get('/user/getbyid/:id', validatorToken ,async function (req, res) {
+app.get('/user/getbyid/:id', validatorToken, async function (req, res) {
     let id = req.params.id;
     console.log(id);
-    try{
-        const user = await User.findById({ _id: id }, 'name email role google address')
-            .populate({ path:"role",
-                        populate: { path: 'group'}
-                    })
-            .skip(skip)
-            .limit(limit)
+    try {
+        const user = await User.findById(id, 'name email role google addresses')
+            .populate({
+                path: "role",
+                populate: { path: 'group' }
+            })
             .exec();
+        if (user) {
+            const addresses = await Address.find({ user: user._id })
+                .exec();
+            if (addresses) {
+                user.addresses = addresses;
+            }
+        }
+        console.log(user);
         res.status(400).json({
             ok: true,
             user
         })
-    }catch (err) {
+    } catch (err) {
         res.status(400).json({
             ok: false,
             err: {
@@ -121,21 +122,21 @@ app.get('/user/getbyid/:id', validatorToken ,async function (req, res) {
             }
         })
     }
-    
+
 });
-app.get('/user/delete/:id', validatorToken,  async function (req, res) {
+app.get('/user/delete/:id', validatorToken, async function (req, res) {
     console.log('asdasdas');
     let id = req.params.id;
     let body = { statuss: false }
     try {
         User.findByIdAndUpdate(id, body, { new: true })
-            .then(user   => {
+            .then(user => {
                 if (user) {
                     res.json({
                         ok: true,
                         user,
                     })
-                }else{
+                } else {
                     res.json({
                         ok: false,
                         err: {
@@ -154,14 +155,14 @@ app.get('/user/delete/:id', validatorToken,  async function (req, res) {
 
     }
     catch (err) {
-    res.json({
-        ok: false,
-        err: {
-            message: 'Hubo un error',
-            err
-        }
-    })
-}
+        res.json({
+            ok: false,
+            err: {
+                message: 'Hubo un error',
+                err
+            }
+        })
+    }
 });
 
 
